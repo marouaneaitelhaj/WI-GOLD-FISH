13aa49d376988589f7e011a228d487d3de56c04e
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-12-09 18:40:14 +0100

    Implement CRUD operations for Competition and Fish services

--- src/main/java/com/WI/WIGOLDFISH/services/impl/CompetitionServiceImpl.java
+++ src/main/java/com/WI/WIGOLDFISH/services/impl/CompetitionServiceImpl.java
@@ -1,38 +1,64 @@
 package com.WI.WIGOLDFISH.services.impl;
 
+import com.WI.WIGOLDFISH.entities.competition.Competition;
 import com.WI.WIGOLDFISH.entities.competition.CompetitionDtoReq;
 import com.WI.WIGOLDFISH.entities.competition.CompetitionDtoRes;
+import com.WI.WIGOLDFISH.exceptions.ResourceNotFound;
+import com.WI.WIGOLDFISH.repositories.CompetitionRepository;
 import com.WI.WIGOLDFISH.services.interfaces.CompetitionService;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class CompetitionServiceImpl implements CompetitionService {
+    @Autowired
+    private CompetitionRepository competitionRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public CompetitionDtoReq save(CompetitionDtoReq dtoMini) {
-        return null;
+        Competition competition = modelMapper.map(dtoMini, Competition.class);
+        competition = competitionRepository.save(competition);
+        return modelMapper.map(competition, CompetitionDtoReq.class);
     }
 
     @Override
     public CompetitionDtoReq update(CompetitionDtoReq dtoMini, String s) {
-        return null;
+        Competition competition = modelMapper.map(dtoMini, Competition.class);
+        competition.setCode(s);
+        competitionRepository.findById(s).orElseThrow(() -> new ResourceNotFound("Competition not found"));
+        competition = competitionRepository.save(competition);
+        return modelMapper.map(competition, CompetitionDtoReq.class);
     }
 
     @Override
     public Boolean delete(String s) {
-        return null;
+        competitionRepository.findById(s).orElseThrow(() -> new ResourceNotFound("Competition not found"));
+        competitionRepository.deleteById(s);
+        if (competitionRepository.findById(s).isPresent()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
     public CompetitionDtoRes findOne(String s) {
-        return null;
+        Competition competition = competitionRepository.findById(s).orElseThrow(() -> new ResourceNotFound("Competition not found"));
+        return modelMapper.map(competition, CompetitionDtoRes.class);
     }
 
     @Override
     public List<CompetitionDtoRes> findAll() {
-        return null;
+        return competitionRepository.findAll().stream().map(competition -> modelMapper.map(competition, CompetitionDtoRes.class)).toList();
     }
 
     @Override
--- src/main/java/com/WI/WIGOLDFISH/services/impl/FishServiceImpl.java
+++ src/main/java/com/WI/WIGOLDFISH/services/impl/FishServiceImpl.java
@@ -1,36 +1,65 @@
 package com.WI.WIGOLDFISH.services.impl;
 
+import com.WI.WIGOLDFISH.entities.fish.Fish;
 import com.WI.WIGOLDFISH.entities.fish.FishDtoReq;
 import com.WI.WIGOLDFISH.entities.fish.FishDtoRes;
+import com.WI.WIGOLDFISH.exceptions.ResourceNotFound;
+import com.WI.WIGOLDFISH.repositories.FishRepository;
+import com.WI.WIGOLDFISH.repositories.LevelRepository;
 import com.WI.WIGOLDFISH.services.interfaces.FishService;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public class FishServiceImpl implements FishService {
+    @Autowired
+    private FishRepository fishRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private LevelRepository levelRepository;
     @Override
     public FishDtoReq save(FishDtoReq dtoMini) {
-        return null;
+        levelRepository.findById(dtoMini.getLevel_id()).orElseThrow(() -> new ResourceNotFound("Level not found"));
+        Fish fish = modelMapper.map(dtoMini, Fish.class);
+        fish = fishRepository.save(fish);
+        return modelMapper.map(fish, FishDtoReq.class);
     }
 
     @Override
     public FishDtoReq update(FishDtoReq dtoMini, String s) {
-        return null;
+        fishRepository.findById(s).orElseThrow(() -> new ResourceNotFound("Fish not found"));
+        levelRepository.findById(dtoMini.getLevel_id()).orElseThrow(() -> new ResourceNotFound("Level not found"));
+        Fish fish = modelMapper.map(dtoMini, Fish.class);
+        fish.setName(s);
+        fish = fishRepository.save(fish);
+        return modelMapper.map(fish, FishDtoReq.class);
     }
 
     @Override
     public Boolean delete(String s) {
-        return null;
+        fishRepository.findById(s).orElseThrow(() -> new ResourceNotFound("Fish not found"));
+        fishRepository.deleteById(s);
+        if (fishRepository.findById(s).isPresent()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
     public FishDtoRes findOne(String s) {
-        return null;
+        Fish fish = fishRepository.findById(s).orElseThrow(() -> new ResourceNotFound("Fish not found"));
+        return modelMapper.map(fish, FishDtoRes.class);
     }
 
     @Override
     public List<FishDtoRes> findAll() {
-        return null;
+        List<FishDtoRes> fishDtoRes = fishRepository.findAll().stream().map(fish -> modelMapper.map(fish, FishDtoRes.class)).toList();
+        return fishDtoRes;
     }
 }
